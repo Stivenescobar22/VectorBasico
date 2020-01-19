72b0e55372fba4b873522a9bbf0dd12deeb595bc
stiven escobar galeano <estibenes@gmail.com>
2020-01-19 13:30:47 -0500

    se suman todos los elementos del vector

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<HTML>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset= utf-8">
+        <title>Suma de valores de un vector</title>
+    
+    </head>
+    <body>
+        
+        <?php 
+        
+           $vector = array(31 893 1025 834 1254 315 931 762 865 1187
+              437 756 371 188 964 639 551 1284 797 838 1263 1075 1172 
+              113 723 185 575 281 306 753 561 395);
+
+              echo "suma del primer vector: " . array_sum($vector);
+        
+        
+        ?>
+      
+     
+    </body>
+      
+</html>
